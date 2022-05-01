699dfa5676e3db2bfa55479c0cc6d05921151807
leoncoci <2461930154@qq.com>
2022-05-01 14:57:28 +0800

    asm

--- include/arch/loongarch/arch/asm/linkage.h
+++ include/arch/loongarch/arch/asm/linkage.h
@@ -23,11 +23,57 @@
 #define SYM_T_FUNC				STT_FUNC
 
 #define PGDIR_SHIFT		28
+
 #define PGD_ORDER		0
+#define PUD_ORDER		0
+#define PMD_ORDER		0
+#define PTE_ORDER		0
+
+#define PAGE_SHIFT	14
+
+#define PTRS_PER_PMD	((PAGE_SIZE << PMD_ORDER) >> 3)
 
 #define PTRS_PER_PGD	((PAGE_SIZE << PGD_ORDER) >> 3)
 
 #define PMD_SHIFT	(PAGE_SHIFT + (PAGE_SHIFT + PTE_ORDER - 3))
+
+#ifdef __ASSEMBLER__
+#define _ULCAST_
+#else
+#define _ULCAST_ (unsigned long)
+#endif
+
+#define PTRS_PER_PTE	((PAGE_SIZE << PTE_ORDER) >> 3)
+
+#define	_PAGE_HUGE_SHIFT	6  /* HUGE is a PMD bit */
+#define	_PAGE_GLOBAL_SHIFT	6
+#define	_PAGE_HGLOBAL_SHIFT	12 /* HGlobal is a PMD bit */
+
+#define	_PAGE_PRESENT_SHIFT	7
+#define	_PAGE_VALID_SHIFT	0
+
+#define	_PAGE_DIRTY_SHIFT	1
+#define	_PAGE_PRESENT_SHIFT	7
+#define	_PAGE_WRITE_SHIFT	8
+#define	_PAGE_MODIFIED_SHIFT	9
+
+/* Default huge tlb size for a given kernel configuration */
+#define PS_HUGE_SIZE   PS_16M
+
+#define _PAGE_PRESENT		(_ULCAST_(1) << _PAGE_PRESENT_SHIFT)
+#define _PAGE_WRITE		(_ULCAST_(1) << _PAGE_WRITE_SHIFT)
+#define _PAGE_DIRTY		(_ULCAST_(1) << _PAGE_DIRTY_SHIFT)
+#define _PAGE_MODIFIED		(_ULCAST_(1) << _PAGE_MODIFIED_SHIFT)
+
+#define HPAGE_SHIFT	(PAGE_SHIFT + PAGE_SHIFT - 3)
+
+#define PAGE_SIZE	(UL_CONST(1) << PAGE_SHIFT)
+
+#define _PAGE_VALID		(_ULCAST_(1) << _PAGE_VALID_SHIFT)
+
+#define _PAGE_HUGE		(_ULCAST_(1) << _PAGE_HUGE_SHIFT)
+
+#define _PAGE_HGLOBAL		(_ULCAST_(1) << _PAGE_HGLOBAL_SHIFT)
 
 #define SYM_END(name, sym_type)				\
 	.type name sym_type ASM_NL			\
