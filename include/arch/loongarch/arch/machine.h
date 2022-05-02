4ee2cf4ac5d84bfcc617aeb879cb06a369e39523
GooTal <1842857470@qq.com>
2022-05-02 21:51:27 +0800

    genex.S related

--- include/arch/loongarch/arch/machine.h
+++ include/arch/loongarch/arch/machine.h
@@ -65,7 +65,7 @@ static inline uint32_t read_cpucfg(uint32_t reg)
  * Registers a0, a1, a3 and a4 as passed to the kernel entry by firmware
  */
 unsigned long fw_arg0, fw_arg1, fw_arg2, fw_arg3;
-
+unsigned long kernelsp;
 
 #endif /* !__ASSEMBLER__ */
 
