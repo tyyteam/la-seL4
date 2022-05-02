18d18b9013fdf0dae6770a0e9dd1538f704b8b45
GooTal <1842857470@qq.com>
2022-05-02 22:10:32 +0800

    modified cache.h 5.2

--- include/arch/loongarch/arch/machine/cache.h
+++ include/arch/loongarch/arch/machine/cache.h
@@ -4,5 +4,5 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
-extern void local_flush_icache_range(unsigned long, unsigned long);
-extern void cache_error_setup(void);
+void local_flush_icache_range(unsigned long, unsigned long);
+void cache_error_setup(void);
