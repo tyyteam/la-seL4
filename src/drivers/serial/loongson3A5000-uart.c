66c9766f567565712531cdeeda64b6d1b58ebe08
GooTal <1842857470@qq.com>
2022-05-11 21:38:50 +0800

    5.11

--- src/drivers/serial/loongson3A5000-uart.c
+++ src/drivers/serial/loongson3A5000-uart.c
@@ -21,10 +21,10 @@
 #define UART_REG_LSR_TFE BIT(5)
 #define UART_REG_LSR_DR BIT(0)
 
-long UART0=0x1fe001e0L;
-// #define UART0 0x1fe001e0L will cause warning due to [-Warray-bounds] or [-Wall]
+// long UART0=0x1fe001e0L;
+// #define UART0 0x1fe001e0L --> will cause warning due to [-Warray-bounds] or [-Wall]
 
-#define UART_REG(reg) ((volatile uint8_t *)(UART0 + (reg)))
+#define UART_REG(reg) ((volatile uint8_t *)(UART_PPTR + (reg)))
 #define uart_read_reg(reg) (*(UART_REG(reg)))
 #define uart_write_reg(reg, v) (*(UART_REG(reg)) = (v))
 
@@ -32,9 +32,6 @@ long UART0=0x1fe001e0L;
 void uart_drv_putchar(unsigned char c)
 {
     while( (uart_read_reg(UART_REG_LSR) & UART_REG_LSR_TFE) == 0 );
-    //while(!(*((volatile uint32_t *)0x1fe001e5L) & UART_REG_LSR_TFE));
-    //while(!(*UART_REG(UART_REG_LSR) & UART_REG_LSR_TFE));
-    //*UART_REG(UART_REG_DAT) = (c & 0xff);
     uart_write_reg(UART_REG_DAT,c);
 }
 #endif /* CONFIG_PRINTING */
