f1fb7c288eeb5e76bb89e6084d83d5f519fc851f
chenyangyy <644093581@qq.com>
2022-08-01 17:49:18 +0800

    fix(kernel/libsel4/sel4_arch_include/loongarch64/sel4/sel4_arch): SEL4_MAPPING_LOOKUP_NO_PT
    3A5000虚拟地址格式设计所对应的SEL4_MAPPING_LOOKUP_NO_PT

--- libsel4/sel4_arch_include/loongarch64/sel4/sel4_arch/mapping.h
+++ libsel4/sel4_arch_include/loongarch64/sel4/sel4_arch/mapping.h
@@ -17,4 +17,4 @@ LIBSEL4_INLINE_FUNC seL4_Word seL4_MappingFailedLookupLevel()
     return seL4_GetMR(SEL4_MAPPING_LOOKUP_LEVEL);
 }
 
-#define SEL4_MAPPING_LOOKUP_NO_PT 21
+#define SEL4_MAPPING_LOOKUP_NO_PT 25
