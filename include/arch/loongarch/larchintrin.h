2faedb81094b4a31aaa166b4e1aacb543ca35742
Leoncoci <45042260+Leoncoci@users.noreply.github.com>
2022-08-14 05:10:09 +0800

    test:test for Cparser
      /github/workspace/kernel/include/arch/loongarch/larchintrin.h:49.0-49.0: syntax error: inserting  YSEMI

--- include/arch/loongarch/larchintrin.h
+++ include/arch/loongarch/larchintrin.h
@@ -45,8 +45,7 @@ typedef struct rdtime
 #ifdef __loongarch64
 extern inline __drdtime_t 
 __attribute__ ((__gnu_inline__, __always_inline__, __artificial__)) 
-__builtin_loongarch_rdtime_d (void)
-{
+__builtin_loongarch_rdtime_d (void){
   __drdtime_t drdtime;
   __asm__ volatile (
     "rdtime.d\t%[val],%[tid]\n\t"
