bf358ec90d81b72f088a2ccd9789d0fbf3a93d89
Leoncoci <45042260+Leoncoci@users.noreply.github.com>
2022-08-14 06:51:59 +0800

    test:test for Cparser

--- include/arch/loongarch/larchintrin.h
+++ include/arch/loongarch/larchintrin.h
@@ -43,7 +43,7 @@ typedef struct rdtime
 } __rdtime_t;
 
 #ifdef __loongarch64
-static inline __drdtime_t 
+extern inline __drdtime_t 
 __attribute__ ((__gnu_inline__, __always_inline__, __artificial__))
 __builtin_loongarch_rdtime_d(void)
 {
